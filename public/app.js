27f145b64975b6135a0874f56c592eeec7dbe911
Lance Tse <steam.bao666@gmail.com>
2024-02-09 15:14:55 -0800

    replacechild to replacechildren

--- public/app.js
+++ public/app.js
@@ -62,7 +62,7 @@ const fetchAllItemsJSON = async () => {
 }
 
 const showCharItems = async (char) => {
-    ciContainer.replaceChild();
+    ciContainer.replaceChildren();
     const items = await fetchAllItemsForCharJSON(char.char_id);
     for(let item of items){
         const ciDiv = document.createElement("div");
